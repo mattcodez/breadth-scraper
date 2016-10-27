2a37de8f1849f69426b958964664ee409f8350b8
Matt Molnar <matthew.molnar@gmail.com>
2016-10-27 17:52:41 -0400

    Converting to async/await

--- index.js
+++ index.js
@@ -11,26 +11,22 @@ const pg = require('knex')({
   searchPath: 'knex,public'
 });
 
-function getNextDomainId(){
-  return pg.select('domains.id')
+async function getNextDomainId(){
+  let domains = await pg.select('domains.id')
     .from('domains')
-    .rightOuterJoin('pages', 'domains.id', 'pages.domain')
+    .leftOuterJoin('pages', 'domains.id', 'pages.domain')
     .whereNull('pages.id')
     .orderBy('domains.id', 'asc')
-    .limit(1)
-    .then(domains => {
-      const domain = domains[0];
-      return domain.id;
-    });
+    .limit(1);
+  return domains[0].id;
 }
 
-function startCapture(){
-  captureDomain(getNextDomainId())
-  .then(msg => {
-    console.log(msg);
-    //Capture next domain once we're all done with the previous
-    process.nextTick(startCapture); //Don't want infinite recursion
-  });
+async function startCapture(){
+  let nextDomainId = await getNextDomainId();
+  const msg = await captureDomain(nextDomainId);
+  console.log('MSG', msg);
+  //Capture next domain once we're all done with the previous
+  process.nextTick(startCapture); //Don't want infinite recursion
 }
 
 startCapture(); //Temporary
@@ -41,52 +37,50 @@ function pauseCapture(){
 
 //I'm thinking this won't be run after initial pages are filled
 //for a given domain, we would just pull based on "pages" then
-function captureDomain(domainId){console.log('domainId', domainId)
-  if (!domainId){
-    throw `domainId must be passed to captureDomain, got ${domainId}`;
+async function captureDomain(domainId){console.log('domainId', domainId)
+  if (typeof(domainId) !== 'number'){
+    throw `domainId [number] must be passed to captureDomain, got ${domainId}`;
   }
   //find page, if it doesn't exist, add domain as url
-  return pg.select()
+  let pages = await pg.select('pages.id', 'pages.url', 'domains.domain')
     .from('pages')
-    .innerJoin('domains', 'domains.id', 'pages.domain')
-    .where('domains.id', domainId)
-    .then(pages => {
-      const page = pages[0];
-
-      //Already have a row in "pages" for this id
-      if (page){
-        return capturePage({
-          pageId:page.pages.id,
-          url:   page.pages.url
-        });
-      }
-      else {
-        const url = `http://www.${page.domains.domain}`;
-        return pg('pages')
-        .returning('id')
-        .insert({
-          domain: domainId,
-          url
-        })
-        .then(id => capturePage({pageId:id[0], url}));
-      }
+    .rightOuterJoin('domains', 'domains.id', 'pages.domain')
+    .where('domains.id', domainId);
+  const page = pages[0];
+console.log('pages', pages);
+  //Already have a row in "pages" for this id
+  if (page.pages.id === null){
+    return capturePage({
+      pageId:page.pages.id,
+      url:   page.pages.url
     });
+  }
+  else {
+    const url = `http://www.${page.domains.domain}`;
+    let id = await pg('pages')
+    .returning('id')
+    .insert({
+      domain: domainId,
+      url
+    });
+    capturePage({pageId:id[0], url});
+  }
 }
 
-function capturePage({pageId,url}){
-  return request({url, resolveWithFullResponse:true})
-  .then(res => {
+async function capturePage({pageId,url}){
+  try{
+    let res = await request({url, resolveWithFullResponse:true})
     console.log(url + " Status code: " + res.statusCode);
 
     const $ = cheerio.load(res.body);
-    pg('pages_captures').insert({
+    let id = await pg('pages_captures').insert({
       page: pageId,
       response_code: res.statusCode,
       body: $('body').text()
-    })
-    .then(id => {
-      return `logged site ${url}`;
     });
-  })
-  .catch(err => console.error(err));
+    return `logged site ${url} pages_captures: ${id}`;
+  }
+  catch(err){
+    console.error(err);
+  }
 }
